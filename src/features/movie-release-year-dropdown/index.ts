a98405aa0726db9e300acb5e60f032fefa33acc3
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-08 02:20:15 +0300

    feat: add a movie-release-year-dropdown comp

---
+++ src/features/movie-release-year-dropdown/index.ts
@@ -0,0 +1,1 @@
+export { MovieReleaseYearDropdown } from "./ui/movie-release-year-dropdown/movie-release-year-dropdown";
---
+++ src/features/movie-release-year-dropdown/ui/movie-release-year-dropdown/movie-release-year-dropdown.tsx
@@ -0,0 +1,31 @@
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { memo } from "react";
+import { Dropdown } from "@/shared/ui/dropdown";
+import { movieReleaseYearDropdownOptions } from "../../const/movie-release-year-dropdown-options";
+import { InputWrapper } from "@/shared/ui/form/input-wrapper";
+
+interface MovieReleaseYearDropdownProps {
+  className?: string;
+  value?: string | null;
+  onChange?: (value: string | null) => void;
+}
+
+export const MovieReleaseYearDropdown = memo(
+  (props: MovieReleaseYearDropdownProps) => {
+    const { className } = props;
+
+    return (
+      <InputWrapper
+        id="release-year-dropdown"
+        label="Release years"
+        className={classNames("", {}, [className])}
+      >
+        <Dropdown
+          placeholder="Select release year"
+          data={movieReleaseYearDropdownOptions}
+          {...props}
+        />
+      </InputWrapper>
+    );
+  }
+);
