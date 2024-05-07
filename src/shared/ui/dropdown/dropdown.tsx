50ea06e651a3ea27ab8fb00c728f4831b04a4a89
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-07 16:53:25 +0300

    refactor: remove  extra code from the dropdown

--- src/shared/ui/dropdown/dropdown.tsx
+++ src/shared/ui/dropdown/dropdown.tsx
@@ -4,48 +4,17 @@ import { memo } from "react";
 import { Select, SelectProps } from "@mantine/core";
 import DownArrowIcon from "@/shared/assets/icons/down-arrow-m.svg";
 
-interface DropdownProps extends SelectProps {
+type PickedMantineSelectProps = Pick<
+  SelectProps,
+  "placeholder" | "data" | "style"
+>;
+
+interface DropdownProps extends PickedMantineSelectProps {
   className?: string;
 }
 
 export const Dropdown = memo((props: DropdownProps) => {
-  const { className, ...otherProps } = props;
-
-  const data = [
-    {
-      label: "всем привет",
-      value: "1",
-    },
-    {
-      label: "всем привет  2",
-      value: "2",
-    },
-
-    {
-      label: "всем привет,минчане?",
-      value: "3",
-    },
-    {
-      label: "Thriller",
-      value: "Thriller",
-    },
-    {
-      label: "Fantasy",
-      value: "Fantasy",
-    },
-    {
-      label: "6",
-      value: "6",
-    },
-    {
-      label: "7",
-      value: "7",
-    },
-    {
-      label: "8",
-      value: "8",
-    },
-  ];
+  const { className, data, ...otherProps } = props;
 
   const comboBoxProps = { size: "md" };
 
@@ -54,15 +23,9 @@ export const Dropdown = memo((props: DropdownProps) => {
       withCheckIcon={false}
       comboboxProps={comboBoxProps}
       data={data}
-      // size="md"
-      // limit={6}
-      // withScrollArea={true}
       className={classNames(cls.dropdown, {}, [className])}
-      // placeholder={placeholder ?? ""}
-      // size={SIZE.MD}
       rightSection={<DownArrowIcon />}
       rightSectionWidth={48}
-      // maxDropdownHeight={267}
       {...otherProps}
     />
   );
