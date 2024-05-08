a46465193aa0cda83896e1a2dd82a1c71d268cc5
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-09 02:45:00 +0300

    feat: add a paginated movie list

--- src/widgets/movie-list-with-filters/index.ts
+++ src/widgets/movie-list-with-filters/index.ts
@@ -1,2 +1,3 @@
 export { MovieFilters } from "./ui/movie-filters/movie-filters";
+export { PaginatedMovieList } from "./ui/paginated-movie-list/paginated-movie-list";
 export { MovieListWithFiltersProvider } from "./providers/ui/movie-list-with-filters-provider";
---
+++ src/widgets/movie-list-with-filters/ui/paginated-movie-list/paginated-movie-list.tsx
@@ -0,0 +1,51 @@
+import { classNames } from "@/shared/lib/classNames/classNames";
+import cls from "./paginated-movie-list.module.scss";
+import { memo, useState } from "react";
+import { Pagination, Stack } from "@mantine/core";
+import { useMovieFilters } from "../../hooks/use-movie-filters";
+import { useMoviesQuery } from "../../api/use-movies-query";
+import { MovieCard } from "@/entities/movie";
+import { MovieCardSize } from "@/entities/movie/ui/movie-card/movie-card";
+
+interface PaginatedMovieListProps {
+  className?: string;
+}
+
+export const PaginatedMovieList = memo((props: PaginatedMovieListProps) => {
+  const { className, ...otherProps } = props;
+  const [page, setPage] = useState<number>(1);
+
+  const { data: filters } = useMovieFilters();
+
+  const { data } = useMoviesQuery(filters, page);
+
+  if (!data) {
+    return "";
+  }
+
+  return (
+    <Stack
+      gap={"1.5rem"}
+      className={classNames(cls.paginatedMovieList, {}, [className])}
+      {...otherProps}
+    >
+      <div className={cls.list}>
+        {data.results.map((movie) => (
+          <MovieCard
+            size={MovieCardSize.M}
+            {...movie}
+            key={movie.id}
+            genres={[]}
+            release_date={movie.release_date as unknown as string}
+          />
+        ))}
+      </div>
+      <Pagination
+        className={cls.pagination}
+        total={data.total_pages}
+        value={page}
+        onChange={setPage}
+      />
+    </Stack>
+  );
+});
