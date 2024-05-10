b2069c60f7206c8e7991f690028fa43f5999f642
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-10 20:42:13 +0300

    feat: add a use-rated-movies hook

--- src/features/movie-rating-card/hooks/use-rated-movies.ts
+++ src/features/movie-rating-card/hooks/use-rated-movies.ts
@@ -1,16 +1,49 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import {
   RatedMoviesContext,
   RatedMoviesState,
 } from "../model/contexts/rated-movies";
 import { RatedMovie } from "../types/movie-rating-card";
+import { usePaginationPage } from "@/app/providers/PaginationPageProvider";
+import { calculatePageCount } from "@/shared/lib/calculate-page-count";
+import { getDataChunk } from "@/shared/lib/get-data-chunk";
 
 interface UseRatedMoviesType {
   movies: RatedMovie[];
+  totalPages: number;
 }
 
-export const useRatedMovies = (): UseRatedMoviesType => {
+export const useRatedMovies = (
+  itemsPerPage: number,
+  movieTitle?: string
+): UseRatedMoviesType => {
   const { movies } = useContext(RatedMoviesContext) as RatedMoviesState;
+  const { page } = usePaginationPage();
 
-  return { movies };
+  const moviesToSplit = useMemo(
+    () => (movieTitle ? findMoviesByTitle(movies, movieTitle) : movies),
+    [movies, movieTitle]
+  );
+
+  const movieChunk = useMemo(
+    () => getDataChunk(moviesToSplit, itemsPerPage, page),
+    [moviesToSplit, itemsPerPage, page]
+  );
+
+  const totalPages = useMemo(
+    () => calculatePageCount(moviesToSplit.length, itemsPerPage),
+    [moviesToSplit]
+  );
+
+  return { movies: movieChunk, totalPages };
+};
+
+const findMoviesByTitle = (movies: RatedMovie[], title?: string) => {
+  if (!title) {
+    return [];
+  }
+
+  return movies.filter((movie) =>
+    movie.original_title.toLowerCase().includes(title.toLowerCase())
+  );
 };
