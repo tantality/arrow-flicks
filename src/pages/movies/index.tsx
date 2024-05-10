2dafa072c137ef86ae02601d84c2f30dade04c5b
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-10 16:35:27 +0300

    feat: add a movies page

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,3 +1,30 @@
-export default function Movies() {
-  return <div>movies</div>;
-}
+import cls from "./movies-page.module.scss";
+import { Sidebar } from "@/widgets/sidebar";
+import { Stack, Title } from "@mantine/core";
+import {
+  MovieFilters,
+  PaginatedMovieList,
+} from "@/widgets/movie-list-with-filters";
+
+const Movies = () => {
+  return (
+    <div className={cls.app}>
+      <div className={cls.appContainer}>
+        <Sidebar className={cls.appSidebar} />
+        <div className={cls.appPage}>
+          <div className={cls.pageContainer}>
+            <Stack gap={"2.5rem"}>
+              <Title order={1}>Movies</Title>
+              <Stack gap={"1.5rem"}>
+                <MovieFilters />
+                <PaginatedMovieList />
+              </Stack>
+            </Stack>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movies;
