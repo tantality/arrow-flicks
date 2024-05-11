3d7b3a1a91826b9e92ab6130f8f0eaf76ad71ff4
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-11 16:54:07 +0300

    feat: change the use-rated-movies hook

--- src/features/movie-rating/hooks/use-rated-movies.ts
+++ src/features/movie-rating/hooks/use-rated-movies.ts
@@ -1,49 +1,10 @@
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import {
   RatedMoviesContext,
   RatedMoviesState,
 } from "../model/contexts/rated-movies";
-import { RatedMovie } from "../types/movie-rating";
-import { usePaginationPage } from "@/app/providers/PaginationPageProvider";
-import { calculatePageCount } from "@/shared/lib/calculate-page-count";
-import { getDataChunk } from "@/shared/lib/get-data-chunk";
 
-interface UseRatedMoviesType {
-  movies: RatedMovie[];
-  totalPages: number;
-}
-
-export const useRatedMovies = (
-  itemsPerPage: number,
-  movieTitle?: string
-): UseRatedMoviesType => {
-  const { movies } = useContext(RatedMoviesContext) as RatedMoviesState;
-  const { page } = usePaginationPage();
-
-  const moviesToSplit = useMemo(
-    () => (movieTitle ? findMoviesByTitle(movies, movieTitle) : movies),
-    [movies, movieTitle]
-  );
-
-  const movieChunk = useMemo(
-    () => getDataChunk(moviesToSplit, itemsPerPage, page),
-    [moviesToSplit, itemsPerPage, page]
-  );
-
-  const totalPages = useMemo(
-    () => calculatePageCount(moviesToSplit.length, itemsPerPage),
-    [moviesToSplit]
-  );
-
-  return { movies: movieChunk, totalPages };
-};
-
-const findMoviesByTitle = (movies: RatedMovie[], title?: string) => {
-  if (!title) {
-    return [];
-  }
-
-  return movies.filter((movie) =>
-    movie.original_title.toLowerCase().includes(title.toLowerCase())
-  );
+export const useRatedMovies = (): RatedMoviesState => {
+  const movies = useContext(RatedMoviesContext) as RatedMoviesState;
+  return movies;
 };
--- src/features/movie-rating/index.ts
+++ src/features/movie-rating/index.ts
@@ -1,2 +1,4 @@
 export { MovieRatingCard } from "./ui/movie-rating-card/movie-rating-card";
 export { RatedMoviesProvider } from "./providers/ui/rated-movies-provider";
+export { useRatedMovies } from "./hooks/use-rated-movies";
+export { type RatedMovie } from "./types/movie-rating";
