918003802f00d6cd098e8aee0443ffdc039df78a
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-11 17:14:03 +0300

    feat: add a rated-movies page

--- src/pages/movies/rated.tsx
+++ src/pages/movies/rated.tsx
@@ -1,3 +1,33 @@
-export default function RatedMovies() {
-  return <div>rated movies</div>;
-}
+import cls from "./movies-page.module.scss";
+import { Sidebar } from "@/widgets/sidebar";
+import { Group, Stack, Title } from "@mantine/core";
+import {
+  MovieFilters,
+  PaginatedMovieList,
+  RatedMovieListWithFiltersProvider,
+} from "@/widgets/rated-movie-list-with-filters";
+
+const RatedMovies = () => {
+  return (
+    <RatedMovieListWithFiltersProvider>
+      <div className={cls.app}>
+        <div className={cls.appContainer}>
+          <Sidebar className={cls.appSidebar} />
+          <div className={cls.appPage}>
+            <div className={cls.pageContainer}>
+              <Stack gap={"2.5rem"}>
+                <Group justify="flex-start" grow>
+                  <Title order={1}>Rated movies</Title>
+                  <MovieFilters />
+                </Group>
+                <PaginatedMovieList />
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RatedMovieListWithFiltersProvider>
+  );
+};
+
+export default RatedMovies;
