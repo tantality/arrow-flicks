6cc2f7261b346aa60f49ff0fe176a70a0c078686
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-11 17:12:38 +0300

    feat: add rated-movie-filters comp

---
+++ src/widgets/rated-movie-list-with-filters/ui/rated-movie-filters/rated-movie-filters.tsx
@@ -0,0 +1,31 @@
+import { memo } from "react";
+import { SearchInput } from "@/features/search-input";
+import { useRatedMovieFilters } from "../../hooks/use-rated-movie-filters";
+import { useRatedMovieFiltersDispatch } from "../../hooks/use-rated-movie-filters-dispatch";
+import { ratedMovieFiltersActions } from "../../model/actions/movie-filters";
+
+interface MovieFiltersProps {
+  className?: string;
+}
+
+export const MovieFilters = memo((props: MovieFiltersProps) => {
+  const { className, ...otherProps } = props;
+
+  const { filters, setTitle } = useRatedMovieFilters();
+  const dispatch = useRatedMovieFiltersDispatch();
+
+  const handleSearchButtonClick = () => {
+    dispatch(ratedMovieFiltersActions.filterRatedMoviesAction(true));
+  };
+
+  return (
+    <div {...otherProps}>
+      <SearchInput
+        placeholder="Search movie title"
+        value={filters.title}
+        onChange={setTitle}
+        onSearch={handleSearchButtonClick}
+      />
+    </div>
+  );
+});
