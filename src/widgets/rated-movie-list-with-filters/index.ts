b6a1a90b5cb5b89280dada42c163aaa2e80a2e3f
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-11 17:13:32 +0300

    feat: add a rated movies list

---
+++ src/widgets/rated-movie-list-with-filters/index.ts
@@ -0,0 +1,3 @@
+export { MovieFilters } from "./ui/rated-movie-filters/rated-movie-filters";
+export { PaginatedMovieList } from "./ui/paginated-movie-list/paginated-movie-list";
+export { RatedMovieListWithFiltersProvider } from "./providers/ui/rated-movie-list-with-filters-provider";
---
+++ src/widgets/rated-movie-list-with-filters/ui/paginated-movie-list/paginated-movie-list.tsx
@@ -0,0 +1,52 @@
+import { classNames } from "@/shared/lib/classNames/classNames";
+import cls from "./paginated-movie-list.module.scss";
+import { memo } from "react";
+import { Grid, GridCol, Stack } from "@mantine/core";
+import { MovieCardSize } from "@/entities/movie/ui/movie-card/movie-card";
+import { Pagination } from "@/shared/ui/pagination";
+import { MovieRatingCard } from "@/features/movie-rating";
+import { usePaginationPage } from "@/app/providers/PaginationPageProvider";
+import { useRatedMoviesChunk } from "../../hooks/use-rated-movies-chunk";
+
+interface PaginatedMovieListProps {
+  className?: string;
+}
+
+const moviesPerPage = 4;
+
+export const PaginatedMovieList = memo((props: PaginatedMovieListProps) => {
+  const { className, ...otherProps } = props;
+
+  const { page, setPage } = usePaginationPage();
+  const { movies, totalPages } = useRatedMoviesChunk(moviesPerPage);
+
+  return (
+    <Stack
+      gap={"1.5rem"}
+      className={classNames(cls.paginatedMovieList, {}, [className])}
+      {...otherProps}
+    >
+      <Grid className={cls.list} gutter={"1rem"}>
+        {movies.map((movie) => (
+          <GridCol span={6} key={movie.id}>
+            <MovieRatingCard
+              isTitleLink={true}
+              className={cls.movieCard}
+              size={MovieCardSize.M}
+              key={movie.id}
+              {...movie}
+            />
+          </GridCol>
+        ))}
+      </Grid>
+      {totalPages > 1 ? (
+        <Pagination
+          className={cls.pagination}
+          total={totalPages}
+          value={page}
+          onChange={setPage}
+        />
+      ) : null}
+    </Stack>
+  );
+});
