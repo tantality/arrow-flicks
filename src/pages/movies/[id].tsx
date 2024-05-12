a7ad50ea3f3e177c304245d30c28f4c184dd613c
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-12 17:50:04 +0300

    feat: implement a movie page

--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -1,7 +1,21 @@
+import { Sidebar } from "@/widgets/sidebar";
+import cls from "./movies-page.module.scss";
 import { useRouter } from "next/router";
+import { MovieDetails } from "@/widgets/movie-details";
 
 export default function Movie() {
   const { id } = useRouter().query;
 
-  return <div>movie with id = {id}</div>;
+  return (
+    <div className={cls.app}>
+      <div className={cls.appContainer}>
+        <Sidebar className={cls.appSidebar} />
+        <div className={cls.appPage}>
+          <div className={cls.pageContainer} style={{ maxWidth: "800px" }}>
+            <MovieDetails movieId={id}></MovieDetails>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
