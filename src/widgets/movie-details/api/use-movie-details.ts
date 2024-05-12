7c549042afc7f9f6a4fedfc225b7f48704b54f88
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-12 17:46:48 +0300

    feat: add a use-movie-details hook

---
+++ src/widgets/movie-details/api/use-movie-details.ts
@@ -0,0 +1,23 @@
+import { MovieDetailsDto } from "@/entities/movie";
+import { axiosInstance } from "@/shared/api/axios";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+export const useMovieDetails = (movieId: number, options?: any) => {
+  const queryRes = useQuery({
+    queryKey: ["movie", movieId],
+    queryFn: () => getMovieDetailsById(movieId),
+    staleTime: Infinity,
+    ...options,
+  }) as UseQueryResult<MovieDetailsDto>;
+
+  return queryRes;
+};
+
+const getMovieDetailsById = async (id: number): Promise<MovieDetailsDto> => {
+  const res = await axiosInstance.get<MovieDetailsDto>(`movie/${id}`, {
+    params: {
+      append_to_response: "videos",
+    },
+  });
+  return res.data;
+};
