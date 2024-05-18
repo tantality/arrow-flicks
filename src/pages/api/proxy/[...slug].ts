b685890c79671bee880e8c7d6fc7be511f2abc20
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-19 01:39:47 +0300

    feat: implement validation of movie filters on a proxy

--- src/pages/api/proxy/[...slug].ts
+++ src/pages/api/proxy/[...slug].ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { API_KEY, APT_URL } from "@/shared/const/api";
+import { validateMovieListQueryParams } from "./lib/validation/movies";
+import { BadRequestException } from "./errors/bad-request";
 
 const proxy = createProxyMiddleware<NextApiRequest, NextApiResponse>({
   target: APT_URL as string,
@@ -14,14 +16,30 @@ const proxy = createProxyMiddleware<NextApiRequest, NextApiResponse>({
   secure: false,
 });
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return new Promise((resolve, reject) => {
-    proxy(req, res, (err: any) => {
-      if (err) {
-        reject(err);
-      }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const isRequestToGetMovieList = req.url?.includes("discover/movie");
+    if (isRequestToGetMovieList) {
+      validateMovieListQueryParams(req);
+    }
 
-      return resolve(res);
+    return new Promise((resolve, reject) => {
+      proxy(req, res, (err: any) => {
+        if (err) {
+          reject(err);
+        }
+
+        return resolve(res);
+      });
     });
-  });
+  } catch (e) {
+    if (e instanceof BadRequestException) {
+      return res.status(e.statusCode).json(e);
+    }
+
+    return res.status(500).json(e);
+  }
 }
