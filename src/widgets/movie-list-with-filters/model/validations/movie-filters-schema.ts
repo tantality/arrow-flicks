e3a74e3cd84b52fc4dbbc291b58490f3e276c0e8
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-18 15:58:29 +0300

    feat: add a movie-filters validation schema

---
+++ src/widgets/movie-list-with-filters/model/validations/movie-filters-schema.ts
@@ -0,0 +1,40 @@
+import { movieReleaseYearDropdownOptions } from "@/features/movie-release-year-dropdown";
+import { MovieSortByValues } from "@/features/movie-sort-by-dropdown";
+import * as Yup from "yup";
+
+const releaseYears = movieReleaseYearDropdownOptions.map(
+  (option) => option.value
+);
+
+export const movieFiltersSchema = Yup.object().shape({
+  genreId: Yup.string()
+  .matches(/^[0-9]+$/, "genreId must be a number")
+  .optional()
+  .nullable(),
+  releaseYear: Yup.string()
+    .matches(/^[0-9]+$/, "releaseYear must be a number")
+    .optional()
+    .nullable()
+    .oneOf(releaseYears),
+  sortBy: Yup.string().oneOf(Object.values(MovieSortByValues)),
+  fromRating: Yup.number()
+    .optional()
+    .transform((value) => (Number.isNaN(value) ? undefined : value))
+    .min(0, "from value must be grater than or equal to 0")
+    .max(10, "from value must be less than or equal to 10"),
+  toRating: Yup.number()
+    .optional()
+    .transform((value) => (Number.isNaN(value) ? undefined : value))
+    .when("fromRating", ([fromRating], schema) => {
+      if (fromRating) {
+        return schema.min(
+          fromRating,
+          "to value must be grater than or equal to from value"
+        );
+      } else
+        return schema.min(0, "to value must be grater than or equal to 0");
+    })
+    .max(10, "to value must be less than or equal to 10"),
+});
+
+export type MovieFiltersYup = Yup.InferType<typeof movieFiltersSchema>;
