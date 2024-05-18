eb89cccea4a16386e9418bf67e75f2ecb5f652db
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-19 01:11:03 +0300

    refactor: delete an extra hook

--- src/widgets/movie-list-with-filters/hooks/use-movie-filters-dispatch.ts
+++
@@ -1,6 +0,0 @@
-import { Dispatch, useContext } from "react";
-import { MovieFiltersDispatchContext } from "../model/contexts/movie-filters";
-import { MovieFiltersActions } from "../model/types/movie-filters";
-
-export const useMovieFiltersDispatch = () =>
-  useContext(MovieFiltersDispatchContext) as Dispatch<MovieFiltersActions>;
