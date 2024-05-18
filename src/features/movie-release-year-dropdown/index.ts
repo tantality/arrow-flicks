6ac14066baacf3d74d05adcfbc998066f79a8fab
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-18 15:35:47 +0300

    feat: configure the movie-release-year-dropdown comp to work with validation and errors

--- src/features/movie-release-year-dropdown/index.ts
+++ src/features/movie-release-year-dropdown/index.ts
@@ -1,1 +1,2 @@
 export { MovieReleaseYearDropdown } from "./ui/movie-release-year-dropdown/movie-release-year-dropdown";
+export { movieReleaseYearDropdownOptions } from "./const/movie-release-year-dropdown-options";
--- src/features/movie-release-year-dropdown/ui/movie-release-year-dropdown/movie-release-year-dropdown.tsx
+++ src/features/movie-release-year-dropdown/ui/movie-release-year-dropdown/movie-release-year-dropdown.tsx
@@ -3,29 +3,37 @@ import { memo } from "react";
 import { Dropdown } from "@/shared/ui/dropdown";
 import { movieReleaseYearDropdownOptions } from "../../const/movie-release-year-dropdown-options";
 import { InputWrapper } from "@/shared/ui/form/input-wrapper";
+import { FieldPath, Control, FieldValues } from "react-hook-form";
 
-interface MovieReleaseYearDropdownProps {
+interface MovieReleaseYearDropdownProps<T extends FieldValues> {
   className?: string;
   value?: string | null;
   onChange?: (value: string | null) => void;
+  error?: string;
+  name?: FieldPath<T>;
+  control?: Control<T>;
 }
 
-export const MovieReleaseYearDropdown = memo(
-  (props: MovieReleaseYearDropdownProps) => {
-    const { className } = props;
+const CustomMovieReleaseYearDropdown = <T extends FieldValues>(
+  props: MovieReleaseYearDropdownProps<T>
+) => {
+  const { className } = props;
 
-    return (
-      <InputWrapper
-        id="release-year-dropdown"
-        label="Release year"
-        className={classNames("", {}, [className])}
-      >
-        <Dropdown
-          placeholder="Select release year"
-          data={movieReleaseYearDropdownOptions}
-          {...props}
-        />
-      </InputWrapper>
-    );
-  }
-);
+  return (
+    <InputWrapper
+      id="release-year-dropdown"
+      label="Release year"
+      className={classNames("", {}, [className])}
+    >
+      <Dropdown
+        placeholder="Select release year"
+        data={movieReleaseYearDropdownOptions}
+        {...props}
+      />
+    </InputWrapper>
+  );
+};
+
+export const MovieReleaseYearDropdown = memo(
+  CustomMovieReleaseYearDropdown
+) as typeof CustomMovieReleaseYearDropdown;
