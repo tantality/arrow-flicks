8a3b0c1de3056f984b79e44956a25fca3ea1b500
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-19 22:00:52 +0300

    feat: add a movie-card-description-list skeleton

--- src/entities/movie/ui/movie-card-description-list/movie-card-description-list.tsx
+++ src/entities/movie/ui/movie-card-description-list/movie-card-description-list.tsx
@@ -4,16 +4,50 @@ import { memo } from "react";
 import { MovieCardDescriptionListItem } from "../../model/types/movie-details";
 import React from "react";
 import { MovieCardSize } from "../movie-card/movie-card";
+import { Skeleton } from "@mantine/core";
 
 interface MovieCardDescriptionListProps {
   className?: string;
+  isLoading?: boolean;
   items: MovieCardDescriptionListItem[];
   cardSize: MovieCardSize;
 }
 
 export const MovieCardDescriptionList = memo(
   (props: MovieCardDescriptionListProps) => {
-    const { className, items, cardSize, ...otherProps } = props;
+    const {
+      className,
+      items,
+      cardSize,
+      isLoading = false,
+      ...otherProps
+    } = props;
+
+    if (!items.length && !isLoading) {
+      return null;
+    }
+
+    if (isLoading) {
+      const skeletonAmount = cardSize === MovieCardSize.L ? 5 : 1;
+      const arr = Array(skeletonAmount).fill(skeletonAmount);
+      return (
+        <div
+          className={classNames(cls.movieCardDescriptionList, {}, [
+            className,
+            cls[cardSize],
+            cls.skeleton,
+          ])}
+          {...otherProps}
+        >
+          {arr.map((_, ind) => (
+            <React.Fragment key={ind}>
+              <Skeleton className={cls.itemName} height={"20px"} />
+              <Skeleton className={cls.itemValue} height={"20px"} />
+            </React.Fragment>
+          ))}
+        </div>
+      );
+    }
 
     return (
       <div
