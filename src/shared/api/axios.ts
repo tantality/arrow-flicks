d46ef12caa0e7e98e85d0a650873764a80297897
Anhelina Tratsiak <angel.tretyak@mail.ru>
2024-05-23 14:27:46 +0300

    feat: add headers

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { API_KEY } from "../const/api";
 
 export const axiosInstance = axios.create({
   timeout: 4000,
   headers: {
-    Authorization : 'Bearer ' + API_KEY
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
   }
 });
